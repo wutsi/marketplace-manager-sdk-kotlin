ef87ab42bfdcc2bf4850f7ef9ff2aea8297b30a7
wutsi <herve.tchepannou@wutsi.com>
2023-01-02 23:04:28 -0500

    fix schema

--- src/main/kotlin/com/wutsi/marketplace/manager/dto/CreateDiscountRequest.kt
+++ src/main/kotlin/com/wutsi/marketplace/manager/dto/CreateDiscountRequest.kt
@@ -3,11 +3,9 @@ package com.wutsi.marketplace.manager.dto
 import java.time.OffsetDateTime
 import kotlin.Boolean
 import kotlin.Int
-import kotlin.Long
 import kotlin.String
 
 public data class CreateDiscountRequest(
-  public val storeId: Long = 0,
   public val name: String = "",
   public val type: String = "",
   public val rate: Int = 0,
